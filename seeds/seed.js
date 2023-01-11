0d7805165285c3734f1b5ad89f1694da5377ea80
david <dmancini87@gmail.com>
2023-01-10 21:24:56 -0500

    changed prjocet in seeds to book

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Book } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const bookData = require('./bookData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const project of bookData) {
+    await Book.create({
+      ...book,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
