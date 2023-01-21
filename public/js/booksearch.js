23676ffe258aecdc5dc69cdb9ccc3f121510a7b7
ezmaa <ezinmark@gmail.com>
2023-01-21 15:55:19 -0500

    new booksearch.js file to add books to page from html, deleted old api get request folders, renamed any profile routes to the proper naming structure.

---
+++ public/js/booksearch.js
@@ -0,0 +1,28 @@
+async function getBooks(input) {
+    // const input = searchInput.textContent;
+    const result = await fetch("https://openlibrary.org/search.json?q=" + input);
+    const books = await result.json();
+    return books;
+}
+
+async function getBookByIsbn(isbn) {
+    const result = await fetch(`http://openlibrary.org/books.json?q=${isbn}`);
+    return result;
+}
+
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+
+searchBtn.addEventListener('click', async function(event) {
+    event.preventDefault();
+    if(searchInput.value) {
+       const searchedBook = await getBooks(searchInput.value);
+        console.log("look here", searchedBook);
+    } else {
+        return ('Failed to Load Books!');
+    } 
+});
+
+function renderBooks() {
+
+}
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,11 @@
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
+    return date?.toLocaleDateString();
   },
   format_amount: (amount) => {
     // format large numbers with commas
-    return parseInt(amount).toLocaleString();
+    return parseInt(amount)?.toLocaleString();
   },
   get_emoji: () => {
     const randomNum = Math.random();
