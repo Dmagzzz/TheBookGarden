1c9c1fde048cfa3175ea43586fa61e1566cbfb5d
Dillon Maglio <hddod15@gmail.com>
2023-01-26 22:25:17 -0500

    fixing api and making it actually work

--- public/js/booksearch.js
+++ public/js/booksearch.js
@@ -1,41 +1,32 @@
-searchBtn.addEventListener('click', async function(event) {
-    event.preventDefault();
-    
-    const test = document.querySelector('.hello');
-    const input = document.querySelector('#searchInput');
+function renderBook(book) {
+    let bookTemplet = `<div class="row mb-2">
+    <div class="col-md-8">
+      <h4><a href="https://openlibrary.org/${book.key}/${book.title}" target="_blank">${book.title}</a></h4>
+    </div>`;
 
-    const result = await fetch("https://openlibrary.org/search.json?q=" + input.value);
-    const books = await result.json();
-    const renderBook = books.docs;
-    console.log(books)
-    console.log(renderBook, "look herree")
-
-   
-    for(i = 0; i < renderBook.length; i++) {
-    
-        const theBook = {
-            title: renderBook[i].title,
-            author: renderBook[i].author_name[0],
-            isbn: renderBook[i].isbn[0],
-            description: renderBook[i].first_sentence[0],
-        }
-         console.log("this is the book", theBook)
-         const jsonBook = JSON.stringify(theBook);
-    // console.log(input?.value, "look here");
-    // console.log(event?.target, "event");
-    
-    
-    
-        
-        // test.innerHTML = JSON.stringify(books);
-        // const title = document.createElement('p');
-        // console.log("Title", books.docs[i].title);
-        test.append(jsonBook);
-        
-       
-
+    if (book.isbn && book.isbn[0]) {
+        bookTemplet += `<img style="width: 200px" src="https://covers.openlibrary.org/b/isbn/${book.isbn[0]}-M.jpg?default=false" alt="" />`;
     }
 
+    bookTemplet += `<div class="col-md-4">
+      <button class="btn btn-sm btn-danger" data-id="${book.isbn && book.isbn[0] ? book.isbn[0] : 'N/A'}">DELETE</button>
+    </div>
+  </div>`;
+
+    return bookTemplet;
+}
+
+searchBtn.addEventListener('click', async function (event) {
+    event.preventDefault();
+
+    const input = document.querySelector('#searchInput');
+    const bookListContainer = document.getElementById('bookList');
+
+    const response = await fetch("https://openlibrary.org/search.json?q=" + input.value);
+    const data = await response.json();
+    const books = data.docs;
+
+    books.forEach(book => bookListContainer.innerHTML += renderBook(book));
 });
 
 
